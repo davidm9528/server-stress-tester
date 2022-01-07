34c67ad44b1e82802a0ee83618bf1aadefbbea01
davidm9528 <davidm9528@gmail.com>
2022-01-07 00:50:32 +0000

    Displaying the number of bytes being sent

--- src/server.py
+++ src/server.py
@@ -2,9 +2,12 @@ from ctypes import addressof
 import socket
 import time
 
+#
+#CHANGE PORT ON RUNS ON BOTH FILES (CLIENT)
+#
 if __name__ == "__main__":
     ip = "0.0.0.0"
-    port = 3344
+    port = 4455
     buffer = 102400
 
 #IPV4, TCP, create socket and bind to address
@@ -35,6 +38,8 @@ totalrcvs = 0
 while True:
     client, address = server.accept()
     print(f"Connection Established - {address[0]}:{address[1]}")
+    
+    #Will call this from client.py / not yet implemented
     data, address = server.recvfrom(buffer)
 
     if not data:
@@ -45,6 +50,10 @@ while True:
         data = len(data)
         totalbytes += data
         totalrcvs += 1
+
+#displayign number of bytes sent kbps
+        rate = totalbytes/(finishedstamp - timestamp) * 8 / 1000
+        print("\nRcvd: %s bytes, %s total in %s s at %s kbps") % (data, totalbytes, finishedstamp - timestamp, rate)
 
     #1024 # of bytes server will recieve
     string = client.recv(1024)
