2469364e0c49c6e152710d123bfdf92caa8cbf70
davidm9528 <davidm9528@gmail.com>
2022-01-07 00:14:06 +0000

    Small addition to the formating of the message given to the user

--- src/server.py
+++ src/server.py
@@ -1,20 +1,38 @@
 from ctypes import addressof
 import socket
+import time
 
 if __name__ == "__main__":
-    ip = "127.0.0.1"
-    port = 1234
+    ip = "0.0.0.0"
+    port = 3344
 
-#IPV4, TCP Connection vvvvv
+#IPV4, TCP, create socket and bind to address
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-#Bind ip with port
 server.bind((ip,port))
 
-#Listen for 5 Clients (will refuse if 5+ clients try to connect)
+#listen for 5 Clients (will refuse if 5+ clients try to connect)
 server.listen(5)
 
+#formatting
+print("\n")
+print("-" * 60)
+print("server.py")
+print("-" * 60)
+print  ("Starting TCP receive server...  control-break to exit.")
+print  ("\nWaiting for data...")
+print("-" * 60)
+
+# total bytes recieved since last 'reset'
+totalbytes = 0
+
+# -1 is a deliberately invalid timestamp
+timestamp = -1
+
+# the total number of bursts that have come in
+totalrcvs = 0
+
 while True:
+
     client, address = server.accept()
     print(f"Connection Established - {address[0]}:{address[1]}")
 
