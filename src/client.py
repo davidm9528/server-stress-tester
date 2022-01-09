cde828c3a3498d7008cfe96108541e2ff38bd20a
davidm9528 <davidm9528@gmail.com>
2022-01-09 21:38:54 +0000

    Added further formatting Fixed the send function, to send encoded Bytes rather than sending simple Strings, making the program functionable

--- src/client.py
+++ src/client.py
@@ -1,58 +1,60 @@
-import socket
+from socket import *
 import string
 import time
 
-if __name__ == "__main__":
-    ip = "127.0.0.1"
-    port = 9988
+ip = "127.0.0.1"
+port = 1010
 
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serverudp = socket(AF_INET,SOCK_DGRAM)
     
-    #Connect with above IP and Port
-    server.connect((ip,port))
+#Connect with above IP and Port
+serverudp.connect((ip,port))
 
-    #formatting
-    print("\n")
-    print("-" * 60)
-    print("client.py")
-    print("-" * 60)
-    print  ("Starting client, press ctrl+c to end.")
-    print  ("\nTarget: ")
-    print  ("server.py running on %s port %s" % (ip, port))
-    print("-" * 60)
+#formatting
+print("\n")
+print("-" * 60)
+print("client.py")
+print("-" * 60)
+print("Starting client, press ctrl+c to end.")
+print("\nTarget: ")
+print("server.py running on %s port %s" % (ip, port))
+print("-" * 60)
 
-    while (True):
-        data = input('% ')
-        args = data.split()
 
-        try: 
-                if args[0] == "reset":
+while (1):
+    data = input('% ')
+    args = data.split()
+
+    try: 
+            if args[0] == "reset":
                     data = "X"
                     numtimes = 1
-                else:
+            else:
                     data = "X" * int(args[0])
                     numtimes = int(args[1])
-        except:
+    except:
                 data = None
                 numtimes = None
                 print("Error, you need to specify two numbers.\n- Number of bytes to send\n- Number of times to send them")
 
-        if not data: 
-            pass
-        else:
-                try:
-                    server.send(bytes("X", (ip, port)))
-
-                    for x in range(numtimes):
-                        if(server.send(data,(ip,port))):
-                            print("*")
-                        else:
-                            print(".")
-
-                            #added slight sleep just so i dont overload the socket
-                            time.sleep(0.1)
-                            print("Finished")
-                            server.close()
-                except:
-                    print("Send failed!")
-                    server.close()
+    if not data: 
+        pass
+    else:
+            try:
+                data_bytes = bytes(data, 'ascii')
+                serverudp.send(data.encode())
+                
+                for x in range(numtimes):
+                    if(serverudp.send(data.encode())):
+                        print("*", end = ' ')
+                      #  time.sleep(0.5)
+                        
+                    elif x not in range(numtimes):
+                        print(".")
+                        #added slight sleep just so i dont overload the socket (.5 seconds)
+                        time.sleep(0.5)
+                        print("Done")
+                        
+            except:
+                print("Send failed!")
+    serverudp.close()
