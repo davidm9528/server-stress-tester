ec42e0aeab8f53402d68511c75334987b994c634
davidm9528 <davidm9528@gmail.com>
2022-01-17 22:44:03 +0000

    UDP protocol switched to TCP Server name change for ease Removal of unnecessary comments and function calls Removal of unnecessary imports

--- src/server.py
+++ src/server.py
@@ -7,16 +7,21 @@ from datetime import datetime
 #CHANGE PORT ON RUNS ON BOTH FILES (CLIENT)
 #
 
-ip = "localhost"
+ip = "127.0.0.1"
 port = 1010
 buffer = 102400
 
 #create socket and bind to address
-serverudp = socket.socket(AF_INET,SOCK_DGRAM)
-serverudp.bind((ip,port))
+#tcp 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.bind((ip,port))
 
 #listen for 1 Clients (will refuse if 1+ clients try to connect)
-#serverudp.listen(1)
+s.listen(1)
+
+conn, address = s.accept()
+
 
 #formatting
 time.time()
@@ -24,10 +29,9 @@ print("\n")
 print("-" * 60)
 print("server.py")
 print("-" * 60)
-print  ("Starting UDP receive server...  control-break to exit.")
+print  ("Starting TCP receive server...  control-break to exit.")
 print  ("\nWaiting for data (client.py)...")
 print("-" * 60)
-
 
 # total bytes recieved since last 'reset'
 totalbytes = 0
@@ -35,8 +39,6 @@ totalbytes = 0
 timestamp = -1
 # the total number of bursts that have come in
 totalrcvs = 0
-
-data, address = serverudp.recvfrom(buffer)
 
 information = socket.getaddrinfo(ip, port, family=AF_INET, proto=AF_INET)
 
@@ -47,10 +49,8 @@ print("Current Timestamp : ", timestampStr)
 print("Server Info: ", information)
 print("-" * 60)
 while (1):
-    
-    #will call this from client.py / not yet implemented (hehe)
-    data, address = serverudp.recvfrom(buffer)
-    #print(f"Connection Established - {address[0]}:{address[1]}")
+
+    data, address = conn.recvfrom(buffer)
 
     if not data:
         print("No data entered")
@@ -68,7 +68,7 @@ while (1):
         rate = totalbytes/(finishedstamp - timestamp) * 8 / 1000
 
         print("Rcvd: %s bytes, %s total in %ss at %skbps" % (data, totalbytes, finishedstamp - timestamp, rate))
-
+        
         #Send 1 single byte to clear stats
         #reset
         if data == True:
@@ -76,4 +76,4 @@ while (1):
             timestamp = time.time()
             totalrcvs = 0
             print("*Reset* - clearing stats.")
-serverudp.close()
+s.close()
