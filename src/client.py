400fd9dcd9ca228de2c01c0388dafcd311879531
davidm9528 <davidm9528@gmail.com>
2022-03-29 23:55:22 +0100

    Script moved into Main method

--- src/client.py
+++ src/client.py
@@ -1,9 +1,8 @@
-
 import socket, time, sys, datetime
 from socket import *
-#import pyshark
 import os
-#import pycurl
+#import networkx as nx
+#import matplotlib.pyplot as plt
 
 #REQUESTS
 file0 = open("txt/davidrequest0.txt")
@@ -35,137 +34,136 @@ port = 1010
 #Wireshark filter
 #ip.addr ==  143.117.100.224 and tcp.port == 6011
 
-#Create the socket
+#Create the client socket
 client = socket(AF_INET, SOCK_STREAM)
 
-#Formatting
 startOfCon = time.perf_counter()
 
 client.connect((ip, port))
 
-print("\n")
-print("-" * 60)
-print("Connection time (seconds): %s" % (time.perf_counter() - startOfCon))
-print("client.py")
-print("-" * 60)
-print("Target: ")
-print("Server is running on %s port %s" % (ip, port))
-print("-" * 60)
-print("Please type which request to send:\nI.e. req1 5\nwill send the 1st request 5 times:\n\n- req0 #\n- req1 #\n- req2 #")
-print("- reset - clears stats")
-print("-" * 20)
+'''
+Using NetworkX library, show a graph of the network traffic on the sockets being used
 
-#def socket_error_handler(exception, socket):
-while True:
-    choice = input('\n% ')
-    args = choice.split()
-    #msg = s.recv(1024)
-    try:
-        if args[0] == "reset":
-            choice = "X"
-            numtimes = 1
-        
-        elif args[0] == "req0":
-            choice = req0
-            numtimes = int(args[1])
-            
-            
-        elif args[0] == "req1":
-            choice = req1
-            numtimes = int(args[1])
-        
-        elif args[0] == "req2":
-            choice = req2
-            numtimes = int(args[1])
-            
-        elif args[0] == "exit":
-            client.close()
-            
-        else: #valid option, but not any of the requests selected  (improved error handling, rather than crashing the system)
-            print("You have just sent one byte, this is not a valid request.")
-            choice = mock 
-            numtimes = 1 #Just sends mock once, enter more user validation
-    except:
-        choice = None
-        numtimes = None
-        print("Error, you need to specify two correct inputs.\n- Which request to send\n- Number of times to send it")
+def graphsock():
+    G = nx.DiGraph()
+    G.add_edge(client.getsockname(), client.getpeername())
+    nx.draw_networkx(G, with_labels=True)
+    plt.show()
+'''          
+def main():
+    print("\n")
+    print("-" * 60)
+    print("Connection time (seconds): %s" % (time.perf_counter() - startOfCon))
+    print("client.py")
+    print("-" * 60)
+    print("Target: ")
+    print("Server is running on %s port %s" % (ip, port))
+    print("-" * 60)
+    print("Please type which request to send:\nI.e. req1 5\nwill send the 1st request 5 times:\n\n- req0 #\n- req1 #\n- req2 #")
+    print("- reset - clears stats")
+    print("-" * 20)
 
-    if not choice:
-        pass
-    else:
+    while True:
+        choice = input('\n% ')
+        args = choice.split()
+        
         try:
-            counter = 0
-            s_start = time.perf_counter()
-            for X in range(numtimes):
-                    counter+=1
-                    #client.dup()
-                    print("*", sep='', end='', flush=True)
-                    
-                    if client.sendall(choice.encode()):
-                        print("test")
-
-                    elif X not in range(numtimes):
-                            print(".")
-                            print("Done")
-                            
-            print("\n")
-            s_end = time.perf_counter()
+            if args[0] == "reset":
+                choice = "X"
+                numtimes = 1
             
-            r_start = time.perf_counter()         
-            databytes = client.recv(17185)
-            r_end = time.perf_counter()
-
-            #buffer needs looked at
+            elif args[0] == "req0":
+                choice = req0
+                numtimes = int(args[1])
+                
+                
+            elif args[0] == "req1":
+                choice = req1
+                numtimes = int(args[1])
             
-            '''
-            define a function that checks if all the databytes have been received, if not, it will keep trying to receive
-            Once it has received all the databytes, it will print the databytes and the time it took to receive them
-            '''
-            def datarecv(databytes):
-                if not databytes:
-                    print("No data received")
-                else:
-                    print("Data received:\n %s" % databytes.decode('utf-8'))
-                    
-            datarecv(databytes)
-            
-            if choice != mock:
+            elif args[0] == "req2":
+                choice = req2
+                numtimes = int(args[1])
                 
-                print("-" * 60)
-                '''
-                print(".")
-                time.sleep(0.5)
-                print("..")
-                time.sleep(0.5)
-                print("...")
-                time.sleep(0.5)
-                print("-" * 60)
-                '''
-                reqsize = len(choice)
+            elif args[0] == "exit":
+                client.close()
                 
-                print("-" * 10 + "Stats" + "-" * 10)
-                print(str(counter) + " " + str(args[0]) + " occurance(s) were sent to " + str(ip) + ":" + str(port))    
-                #message showing size of request sent
-                print("Size of rquest sent to server: " + str((reqsize*numtimes)) + " bytes")  
-                print("Size of response from server: "+ str(len(databytes)), "bytes")
-                
-                send_time = s_end - s_start
-                recv_time = r_end - r_start
-                avg_time = (send_time + recv_time) / 2
-                
-                format_send_time = "{:.7f}".format(send_time)
-                format_recv_time = "{:.7f}".format(recv_time)
-                format_avg_time = "{:.7f}".format(avg_time)
-                roundtime = "{:.7f}".format(send_time + recv_time)
-                
-                print("Time to send %s request(s): %s" % (numtimes, format_send_time) + " seconds")
-                print("Time to receive reply: %s" % (format_recv_time) + " seconds")
-                print("Roundtime: " + roundtime + " seconds")
-                print("Avg: %s" % (float(format_avg_time)) + " seconds")
-                print("-" * 60)
+            else: #valid option, but not any of the requests selected  (improved error handling, rather than crashing the system)
+                print("You have just sent one byte, this is not a valid request.")
+                choice = mock 
+                numtimes = 1 #Just sends mock once, enter more user validation
+        except:
+            choice = None
+            numtimes = None
+            print("Error, you need to specify two correct inputs.\n- Which request to send\n- Number of times to send it")
 
-        except OSError as err:
-            print("-Connection Error-:\n-Please check the below message-\n%s" % err)
-            #print("Send failed!")  
+        if not choice:
+            pass
+        else:
+            try:
+                counter = 0
+                s_start = time.perf_counter()
+                for X in range(numtimes):
+                        counter+=1
+                        #client.dup()
+                        print("*", sep='', end='', flush=True)
+                        
+                        if client.sendall(choice.encode()):
+                            print("test")
+
+                        elif X not in range(numtimes):
+                                print(".")
+                                print("Done")
+                                
+                print("\n")
+                s_end = time.perf_counter()
+                
+                r_start = time.perf_counter()         
+                databytes = client.recv(8820) 
+                #uncomment for qub server vvvv
+                #databytes2 = client.recv(8820)      
+                r_end = time.perf_counter()
+                
+                if choice != mock:
+                    print("-" * 60)
+                    print(databytes.decode('utf-8'))
+                    #print(databytes.decode('utf-8') + datebytes2.decode('utf-8'))
                     
-    #client.close()
+                    print("-" * 60)
+                    print(".")
+                    time.sleep(0.5)
+                    print("..")
+                    time.sleep(0.5)
+                    print("...")
+                    time.sleep(0.5)
+                    
+                    reqsize = len(choice)
+                    
+                    print("-" * 15 + "Stats" + "-" * 15)
+                    print(str(counter) + " " + str(args[0]) + " occurance(s) were sent to " + str(ip) + ":" + str(port))    
+                    print("Size of rquest sent to server: " + str((reqsize*numtimes)) + " bytes")  
+                    print("Size of response from server: "+ str(len(databytes)), "bytes")
+                    
+                    send_time = s_end - s_start
+                    recv_time = r_end - r_start
+                    avg_time = (send_time + recv_time) / 2
+                    
+                    format_send_time = "{:.7f}".format(send_time)
+                    format_recv_time = "{:.7f}".format(recv_time)
+                    format_avg_time = "{:.7f}".format(avg_time)
+                    roundtime = "{:.7f}".format(send_time + recv_time)
+                    
+                    print("Time to send %s request(s): %s" % (numtimes, format_send_time) + " seconds")
+                    print("Time to receive reply: %s" % (format_recv_time) + " seconds")
+                    print("Roundtime: " + roundtime + " seconds")
+                    print("Avg: %s" % (float(format_avg_time)) + " seconds")
+                    #graphsock()
+                    print("-" * 60)
+            
+            except OSError as err:
+                print("-Connection Error-:\n-Please check the below message-\n%s" % err)  
+                        
+        #client.close()
+        
+if __name__ == "__main__":
+    main()
