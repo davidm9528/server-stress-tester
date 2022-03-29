0c6e34d87db2554cafca0481942b7c18ce09ab77
davidm9528 <davidm9528@gmail.com>
2022-03-29 23:56:38 +0100

    Server script moved to main method

--- src/server.py
+++ src/server.py
@@ -4,86 +4,94 @@ import socket
 import threading
 import time
 from datetime import datetime
+from time import perf_counter_ns
+
+ip = "127.0.0.1"
+port = 1010
+buffer = 10000
 
 #
 #CHANGE PORT ON RUNS ON BOTH FILES (CLIENT)
 #
+def main():
+    print("Server running, waiting for connection(s)...")
+    print(ip + ":" + str(port))
 
-filereply = open("txt/reply.txt")
-reply = filereply.read().replace("","")
-filereply.close()
+    filereply = open("txt/reply.txt")
+    reply = filereply.read().replace("","")
+    filereply.close()
 
-ip = "127.0.0.1"
-port = 1010
-buffer = 102400
+    #create socket and bind to address
+    #tcp 
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-#create socket and bind to address
-#tcp 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((ip,port))
 
-s.bind((ip,port))
+    s.listen(0)
 
-#listen for 1 Clients (will refuse if 1+ clients try to connect)
-s.listen(5)
+    conn, address = s.accept()
+    '''
+    def graphsock():
+        G = nx.DiGraph()
+        G.add_edge(s.getsockname(), s.getpeername())
+        nx.draw_networkx(G, with_labels=True)
+        plt.show()
+    '''
+    #formatting
+    time.time()
+    print("\n")
+    print("-" * 60)
+    print("server.py")
+    print("-" * 60)
+    print  ("Starting TCP receive server...  control-break to exit.")
+    print  ("\nWaiting for data (client.py)...")
+    print("-" * 60)
 
-conn, address = s.accept()
+    # total bytes recieved since last 'reset'
+    totalbytes = 0
+    # -1 is a deliberately invalid timestamp
+    timestamp = -1
+    # the total number of bursts that have come in
+    totalrcvs = 0
 
-#formatting
-time.time()
-print("\n")
-print("-" * 60)
-print("server.py")
-print("-" * 60)
-print  ("Starting TCP receive server...  control-break to exit.")
-print  ("\nWaiting for data (client.py)...")
-print("-" * 60)
+    information = socket.getaddrinfo(ip, port, family=AF_INET, proto=AF_INET)
 
-# total bytes recieved since last 'reset'
-totalbytes = 0
-# -1 is a deliberately invalid timestamp
-timestamp = -1
-# the total number of bursts that have come in
-totalrcvs = 0
+    print("-Connection Established-")
+    dateTimeObj = datetime.now()
+    timestampStr = dateTimeObj.strftime("%d-%b-%Y (%H:%M:%S.%f)")
+    print("Current Timestamp : ", timestampStr)
+    print("Server Info: ", information)
+    print("-" * 60)
 
-information = socket.getaddrinfo(ip, port, family=AF_INET, proto=AF_INET)
+    while (1):
+        recvs = time.perf_counter_ns()
+        data, address = conn.recvfrom(buffer)
+        recvf = time.perf_counter_ns()
 
-print("-Connection Established-")
-dateTimeObj = datetime.now()
-timestampStr = dateTimeObj.strftime("%d-%b-%Y (%H:%M:%S.%f)")
-print("Current Timestamp : ", timestampStr)
-print("Server Info: ", information)
-print("-" * 60)
+        if not data:
+            print("***Receiving no more data***")
+            print("Closing...")
+            break
+        else:   
+            finishedstamp = recvf - recvs
+            
+            data = len(data)
+            totalbytes += data
+            totalrcvs += 1
 
+            print("Rcvd: %s bytes, %s total in %sns" % (data, totalbytes, (finishedstamp/1000) - timestamp))
+            #graphsock()
 
-while (1):
+            conn.send(bytes(reply, encoding="utf8"))
 
-    data, address = conn.recvfrom(buffer)
-
-    if not data:
-        print("No data entered")
-        break
-    else:
-        
-        finishedstamp = time.time()
-        
-        data = len(data)
-        totalbytes += data
-        totalrcvs += 1
-
-        #displaying number of bytes sent kbps
-        # * 8 /1000 for 8 bits in a byte
-        rate = totalbytes/(finishedstamp - timestamp) * 8 / 1000
-
-        print("Rcvd: %s bytes, %s total in %ss at %skbps" % (data, totalbytes, finishedstamp - timestamp, rate))
-
-        conn.send(bytes(reply, encoding="utf8"))
-        #works ^
-
-        #Send 1 single byte to clear stats
-        #reset
-        if data == True:
-            totalbytes = 0
-            timestamp = time.time()
-            totalrcvs = 0
-            print("*Reset* - clearing stats.")
+            #Send 1 single byte to clear stats
+            #reset
+            if data == True:
+                totalbytes = 0
+                timestamp = time.perf_counter()
+                totalrcvs = 0
+                print("*Reset* - clearing stats.")
 #s.close()
+
+if __name__ == "__main__":
+    main()
